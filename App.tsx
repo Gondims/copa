5f03395b038b1ad324fd2b8329911efaaa5371e4
Gondims <leticiagondim1.0@gmail.com>
2022-11-14 23:28:50 -0400

    mobile

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, Center, Text} from "native-base";
+import { THEME } from './src/styles/theme'
+// import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Blod  } from '@expo-google-fonts/roboto';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hallouuuu</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <Center flex={1} bgColor="gray.900">
+        <Text color="white" fontSize={24}>
+          Hallouuuu
+        </Text>
+        <StatusBar style="auto" />
+      </Center>
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
